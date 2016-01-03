b44c4675e5407c0bff0b2ae0e50ee9c309e3d52f
Johannes Schriewer <hallo@dunkelstern.de>
2016-01-03 04:13:16 +0100

    Initial commit

---
+++ Package.swift
@@ -0,0 +1,13 @@
+//
+//  Package.swift
+//  BlocksRuntime
+//
+//  Created by Johannes Schriewer on 2016-01-03.
+//  Copyright © 2016 Johannes Schriewer. All rights reserved.
+//
+
+import PackageDescription
+
+let package = Package(
+    name: "BlocksRuntime"
+)
